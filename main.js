9fa636636d9a8c485149f4bee00d29521ef44daa
me <me@example.com>
2020-05-30 17:23:38 +0200

    Add v-if for list of users

--- main.js
+++ main.js
@@ -1,81 +1,59 @@
-var RegForm = {
-    template: `
-    <form>
-        <label for="first-name">Enter your first name:</label>
-        <input type="text" placeholder="Your Name" name="first-name"  required>
-
-        <label for="last-name">Enter your last name:</label>
-        <input type="text" placeholder="Your Last Name" name="last-name"  required>
-        
-        <label for="date-of-birth">Enter your birth date:</label>
-        <input type="date" name="date-of-birth" required>
-
-
-        <div>
-
-            <label for="sex">Select sex:</label>
-            
-            <input type="radio" id="male" name="sex" value="male">
-            <label for="male">Male</label>
-
-            <input type="radio" id="female" name="sex" value="female">
-            <label for="female">Female</label>
-        </div>
-
-
-        <label for="interests">Choose your interests:</label>
-
-        <select name="interests" id="interests" multiple>
-            <option value="sports">Sports</option>
-            <option value="books">Books</option>
-            <option value="politics">Politics</option>
-            <option value="travel">Travel</option>
-            <option value="food">Food</option>
-        </select>
-
-        <button type="submit">Register</button>
-    </form>
-    `
-}
 Vue.component( 'regist-form', {
+    props: ['title'],
     template: `
-    <form>
-        <label for="first-name">Enter your first name:</label>
-        <input type="text" placeholder="Your Name" name="first-name"  required>
+        <form>
+            <label for="first-name">Enter your first name:</label>
+            <input type="text" placeholder="Your Name" name="first-name"  required>
 
-        <label for="last-name">Enter your last name:</label>
-        <input type="text" placeholder="Your Last Name" name="last-name"  required>
-        
-        <label for="date-of-birth">Enter your birth date:</label>
-        <input type="date" name="date-of-birth" required>
-
-
-        <div>
-
-            <label for="sex">Select sex:</label>
+            <label for="last-name">Enter your last name:</label>
+            <input type="text" placeholder="Your Last Name" name="last-name"  required>
             
-            <input type="radio" id="male" name="sex" value="male">
-            <label for="male">Male</label>
-
-            <input type="radio" id="female" name="sex" value="female">
-            <label for="female">Female</label>
-        </div>
+            <label for="date-of-birth">Enter your birth date:</label>
+            <input type="date" name="date-of-birth" required>
 
 
-        <label for="interests">Choose your interests:</label>
+            <div>
 
-        <select name="interests" id="interests" multiple>
-            <option value="sports">Sports</option>
-            <option value="books">Books</option>
-            <option value="politics">Politics</option>
-            <option value="travel">Travel</option>
-            <option value="food">Food</option>
-        </select>
+                <label for="sex">Select sex:</label>
+                
+                <input type="radio" id="male" name="sex" value="male">
+                <label for="male">Male</label>
 
-        <button type="submit">Register</button>
-    </form>
-    `
+                <input type="radio" id="female" name="sex" value="female">
+                <label for="female">Female</label>
+
+            </div>
+
+
+            <label for="interests">Choose your interests (hold CTRL while you choose):</label>
+
+            <select name="interests" id="interests" multiple="multiple">
+                <option value="sports">Sports</option>
+                <option value="books">Books</option>
+                <option value="politics">Politics</option>
+                <option value="travel">Travel</option>
+                <option value="food">Food</option>
+            </select>
+
+            <button type="submit">Register</button>
+        </form>
+        <p>{{ title }}</p>
+        `,
+    methods: {
+        
+    }
 })
+var list = new Vue({
+    el: "#list",
+    data: {
+        users: [
+            { name: "Krzysztof" },
+            { name: "Paweł" },
+            { name: "Konrad" },
+        ]
+    }
+});
+
 
 var app = new Vue({
     el: '#app',
