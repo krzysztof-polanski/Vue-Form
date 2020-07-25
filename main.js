530eaefeff720570647b95c87a7d11e67e1d3cc5
me <me@example.com>
2020-07-25 21:52:13 +0200

    Style page

--- main.js
+++ main.js
@@ -1,28 +1,85 @@
+// Material Select Initialization
+
+
+
 const UsersList = {
     template: 
         `
         <div v-if='list.length'>
-        <p>Registered users:</p>
-        <ol >
+        <h3>Registered users:</h3>
+        <ol>
             <template v-for="user in list">
                 <li :key="user.id">
                     <ul>
-                        <li><strong>Id:</strong> {{ user.id }}</li>
                         <li><strong>Name:</strong> {{ user.name }}</li>
                         <li><strong>Last Name:</strong> {{ user.lastName }}</li>
-                        <li><strong>Born on:</strong> {{ user.birthday }}</li>
+                        <li><strong>Born on:</strong> {{ dateVisual(user.birthday) }}</li>
                         <li><strong>Sex:</strong> {{ user.sex }}</li>
-                        <li><strong>Likes:</strong> {{ user.interests }}</li>
+                        <li>Likes {{ interestsVisual(user.interests) }}</li>
                     </ul>
                 </li>
             </template>
         </ol>
         </div>
         `,
-    props: ['list']
+    props: ['list'],
+    methods: {
+        
+        dateVisual(str){
+
+            isZero = function(n){
+                if (n[0] == 0){
+                    return n[1]
+                }else {
+                    return n
+                }
+            }
+
+            console.log(str);
+            let arr = str.split("-")
+            if (str.split("-")[1] == "01"){
+                return isZero(arr[2]) + " January " + arr[0]
+            }else if (str.split("-")[1] == "02"){
+                return isZero(arr[2]) + " February " + arr[0]
+            }else if (str.split("-")[1] == "03"){
+                return isZero(arr[2]) + " March " + arr[0]
+            }else if (str.split("-")[1] == "04"){
+                return isZero(arr[2]) + " April " + arr[0]
+            }else if (str.split("-")[1] == "05"){
+                return isZero(arr[2]) + " May " + arr[0]
+            }else if (str.split("-")[1] == "06"){
+                return isZero(arr[2]) + " June " + arr[0]
+            }else if (str.split("-")[1] == "07"){
+                return isZero(arr[2]) + " July " + arr[0]
+            }else if (str.split("-")[1] == "08"){
+                return isZero(arr[2]) + " August " + arr[0]
+            }else if (str.split("-")[1] == "09"){
+                return isZero(arr[2]) + " September " + arr[0]
+            }else if (str.split("-")[1] == "10"){
+                return isZero(arr[2]) + " October " + arr[0]
+            }else if (str.split("-")[1] == "11"){
+                return isZero(arr[2]) + " November " + arr[0]
+            }else if (str.split("-")[1] == "12"){
+                return isZero(arr[2]) + " December " + arr[0]
+            }else {
+                return "date was not recognized"
+            }
+        },
+        interestsVisual(arr){
+            console.log(arr);
+            let sentence = "";
+            for(e = 0; e < arr.length - 2; e++){
+                sentence += (arr[e] + ", ")
+            }if (arr.length === 1){
+                return sentence + arr[0] + "."
+            }else{
+                return sentence + arr[arr.length - 2] + " and " + arr[arr.length - 1] + "."
+            }
+        }
+    }
 };
 
-// Vue.component('errors-list', {
+
 const ErrorsList = {
     template: `
         <p v-if="error.length">
@@ -34,7 +91,7 @@ const ErrorsList = {
     props: ['error']
 };
 
-// Vue.component('registerForm', {
+
 const RegisterForm = {
     components: {
         'errors-list': ErrorsList
@@ -44,60 +101,69 @@ const RegisterForm = {
             <form @submit.prevent @submit="formValidate" action="#" method="post">
 
                 
-                <errors-list :error='errors' />
-
-                <label for="newName">Enter your first name:</label>
-                <input v-model="newUser.newName" type="text" placeholder="Type Your Name" name="newName">
-
-                </br>
-
-                <label for="newLastName">Enter your last name:</label>
-                <input v-model="newUser.newLastName" type="text" placeholder="Type Your Last Name" name="newLastName">
-
-                
-                
-                </br>
-                
-                <label for="newBirthday">Choose your birth date:</label>
-                <input v-model="newUser.newBirthday" type="date" name="newBirthday" max="{ new Date() }">
-
-                </br>
+                <errors-list class="errors" :error='errors' />
 
                 <div>
-
-                    <label for="newSex">Select sex:</label>
+                    <label for="newName">Enter your first name:</label>
+                    <input v-model="newUser.newName" type="text" placeholder="Type Your Name" name="newName">
+    
+                    </br>
+    
+                    <label for="newLastName">Enter your last name:</label>
+                    <input v-model="newUser.newLastName" type="text" placeholder="Type Your Last Name" name="newLastName">
+    
                     
-                    <input v-model="newUser.newSex" type="radio" id="male" name="newSex" value="Male">
-                    <label for="male">Male</label>
-
-                    <input v-model="newUser.newSex" type="radio" id="female" name="newSex" value="Female">
-                    <label for="female">Female</label>
-
+                    
+                    </br>
+                    
+                    <label for="newBirthday">Choose your birth date:</label>
+                    <input v-model="newUser.newBirthday" type="date" name="newBirthday" max="{ new Date() }" min="1899-01-01">
                 </div>
 
-                <label for="newInterests">Choose your interests (hold CTRL while you choose):</label>
 
-                <select v-model="newUser.newInterests" name="newInterests" id="interests" multiple>
-                    <option value="sports">Sports</option>
-                    <option value="books">Books</option>
-                    <option value="politics">Politics</option>
-                    <option value="travel">Travel</option>
-                    <option value="food">Food</option>
-                </select>
+                <div>
+                    <label for="newSex">Select sex:</label>
+                    <div class="sex-choice">
+    
+                        
+                        <div class="sex-choice-el">
+                            <input v-model="newUser.newSex" type="radio" id="female" name="newSex" value="Female">
+                            <label class="radio-label" for="female">Female</label>
+                        </div>
+    
+                        <div class="sex-choice-el">
+                            <input v-model="newUser.newSex" type="radio" id="male" name="newSex" value="Male">
+                            <label class="radio-label" for="male">Male</label>
+                        </div>
+    
+                    </div>
+                </div>
 
-                </br>
+                <div class="interests">
+                    <label for="newInterests">Choose your interests (hold CTRL while you choose):</label>
+                    
+                    
+    
+                    <select v-model="newUser.newInterests" name="newInterests" id="interests-list" multiple>
+                        <option value="sports">Sports</option>
+                        <option value="books">Books</option>
+                        <option value="politics">Politics</option>
+                        <option value="travels">Travels</option>
+                        <option value="food">Food</option>
+                    </select>
+                </div>
+
+                
 
                 
                 
-                <input type="submit" v-bind:class="{ 'text-danger': !newUser.newName }" value="Add User">
+                <div><input type="submit" v-bind:class="{ 'submit-ready': newUser.newName && newUser.newLastName && newUser.newBirthday && newUser.newSex && newUser.newInterests.length }" value="Add User"></div>
             </form>
 
 
         </div>        
             
             `,
-            //wycięte na razie
-            // <p v-if="newName">New user is: {{ newName[0] + newLastName }} </p>
     props: [''],
     data: function() {
         return {
@@ -119,7 +185,7 @@ const RegisterForm = {
             this.$emit('add-user-new', this.newUser);
         },
         clearForm() {
-            this.newUser.newId++;
+            this.newUser.newId += 1;
             this.newUser.newName = null;
             this.newUser.newLastName = null;
             this.newUser.newBirthday = null;
@@ -163,7 +229,7 @@ const RegisterForm = {
                 this.errors.push('Are you even born yet? You need to be at least 0 to join us :/');
             }
             if (!this.newUser.newSex) {
-                this.errors.push('If you\'re not sure about your sex, you shouldn\'t be here...');
+                this.errors.push('If you\'re not sure about your sex, you should visit the specialist first...');
             }
             if (!this.newUser.newInterests.length) {
                 this.errors.push('You do have some interests, don\'t you? Just pick already!');
@@ -174,8 +240,6 @@ const RegisterForm = {
     }
 };
 
-// const Form = { template: '<div>form</div>' }
-// const List = { template: '<div>list</div>' }
 
 const routes = [
     { path: '/form', component: RegisterForm },
@@ -189,15 +253,16 @@ const router = new VueRouter({
     ]
 })
 
+
+
 const app = new Vue({
     router,
     // el: '#app',
-    // components: {
-    //     'register-form': RegisterForm,
-    //     'users-list': UsersList
-    // },
+    components: {
+        'register-form': RegisterForm,
+        'users-list': UsersList
+    },
     data: function(){
-        // message: "Test paragraph is working!",
         return {
             users: [
                 // {
@@ -227,6 +292,7 @@ const app = new Vue({
             ],
         }
     },
+    props: ['users.newId'],
     methods: {
         updateList(nU) {
             // this.users1.push(id);
@@ -240,32 +306,24 @@ const app = new Vue({
             });
         }
     }
-    // computed: {
-    //     reversedMessage: function() {
-    //         return this.message.split('').reverse().join('');
-    //     }
-    // },
-    
 }).$mount('#app')
 
 
-// Vue Router - testujemy:
 
 
-// const Form = { template: '<div>form</div>' }
-// const List = { template: '<div>list</div>' }
+//można dodać jako ładniejszy wygląd rozwijanej listy zainteresowań:
+/* <select v-model="newUser.newInterests" name="newInterests" id="interests" multiple data-placeholder="Begin typing a name to filter..." class="chosen-select">
+                    <option value=""></option>
+                    <option value="something1">American Black Bear</option>
+                    <option value="something2">Asiatic Black Bear</option>
+                    <option value="something3">Brown Bear</option>
+                    <option value="something4">Giant Panda</option>
+                    <option value="something5">Sloth Bear</option>
+                    <option value="something6">Sun Bear</option>
+                    <option value="something7">Polar Bear</option>
+                    <option value="something8">Spectacled Bear</option>
+                </select>
 
-
-
-// const routes = [
-//     { path: '/form', component: Form },
-//     { path: '/list', component: List }
-// ]
-
-// const router = new VueRouter({
-//     routes
-// })
-
-// const Rout = new Vue({
-//     router
-// }).$mount('#rout')
+$(".chosen-select").chosen({
+    no_results_text: "Oops, nothing found!"
+  }) */
